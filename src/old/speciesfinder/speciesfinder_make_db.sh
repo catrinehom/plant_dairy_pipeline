06bf3103ad6624c4e0506c72cf53117a300ee14f
catrinehom <catrinehom@gmail.com>
2020-09-18 10:48:09 +0200

    Removed redundant files (templates/old etc)

--- src/old/speciesfinder/speciesfinder_make_db.sh
+++
@@ -1,8 +0,0 @@
-### NEW DB:
-https://www.arb-silva.de/fileadmin/silva_databases/release_138/Exports/SILVA_138_SSUParc_tax_silva.fasta.gz
-
-## Noget med at downloade denne? Og indexere den måske??
-
-SPECIESFINDER_DB=$(/home/projects/cge/people/cathom/data/speciesfinder_db)
-
-
